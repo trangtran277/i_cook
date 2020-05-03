463571e593b712689a9c6478ff3e36ccc7d7e611
Dayeong <day5018@gmail.com>
2020-05-03 17:23:10 +0900

    edited home screen

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget{
+  @override
+  HomeScreenState createState()=>new HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen>{
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text(
-        "home",
-        style: new TextStyle(fontSize: 20.0),
-      ),
-    );
+    return new Scaffold(
+        body: new Stack(
+          children: <Widget>[
+            Positioned(
+              width: 350.0,
+              left: 40.0,
+              top: MediaQuery.of(context).size.height / 20,
+              child: Column(children: <Widget>[
+                Container(width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  image: DecorationImage(
+                    image: NetworkImage("https://picsum.photos/250?image=9"),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(70.0))
+                ),),
+                SizedBox(height: 20.0),
+                Text(
+                  "John Doe",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  )
+                )
+              ],)
+            )
+          ],
+        )
+      );
   }
 }
